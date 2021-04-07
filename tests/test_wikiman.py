d397d4645dcb13e4536ee85528a57cb5a816c1d9
blakeNaccarato <20692450+blakeNaccarato@users.noreply.github.com>
2021-04-06 21:41:22 -0700

    First actual working test

--- tests/test_wikiman.py
+++ tests/test_wikiman.py
@@ -1,7 +1,16 @@
 # pylint: disable=missing-module-docstring, missing-function-docstring, unused-argument
 
+from pathlib import Path
+
+import pytest
 import wikiman as wm
 
+TESTS_ROOT = Path("tests/wiki")
 
-def test_test(wiki_directory):
-    assert 1
+
+@pytest.mark.parametrize("page, expected", [(wm.ROOT_PAGE, wm.ROOT / "Home.md")])
+def test_get_parent(wiki_directory, page, expected):
+
+    result = wm.get_parent(page)
+
+    assert result == expected
