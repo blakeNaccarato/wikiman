bae12c8a9cc82267c9b259e3c685bd7dce7ea2e5
blakeNaccarato <20692450+blakeNaccarato@users.noreply.github.com>
2021-04-07 16:47:18 -0700

    Remove unintentional folder in wikiman src

--- tests/test_wikiman.py
+++ tests/test_wikiman.py
@@ -1,11 +1,32 @@
 # pylint: disable=missing-module-docstring, missing-function-docstring, unused-argument, wrong-import-order
 
+from filecmp import dircmp
+
 import wikiman as wm
 from pytest import mark as m
 
-
 # * -------------------------------------------------------------------------------- * #
 # * FILE OPERATIONS
+
+
+@m.parametrize(
+    "test_id, args",
+    [
+        (
+            "name_has_dashes",
+            ("test-page-name", wm.ROOT_PAGE, 0),
+        ),
+    ],
+)
+def test_create_page(test_id, args, expected_wiki):
+
+    (name, under, position) = args
+    page = wm.init_page(name, under, position)
+    wm.create_page(page)
+
+    result = dircmp(wm.WIKI_ROOT, expected_wiki)
+
+    assert not result.diff_files
 
 
 @m.parametrize(
@@ -14,12 +35,12 @@ from pytest import mark as m
         (
             "name_has_dashes",
             ("test-page-name", wm.ROOT_PAGE, 0),
-            wm.ROOT / "00_test-page-name/test-page-name.md",
+            wm.WIKI_ROOT / "00_test-page-name/test-page-name.md",
         ),
         (
             "name_has_spaces",
             ("test page name", wm.ROOT_PAGE, 0),
-            wm.ROOT / "00_test-page-name/test-page-name.md",
+            wm.WIKI_ROOT / "00_test-page-name/test-page-name.md",
         ),
     ],
 )
@@ -42,7 +63,7 @@ def test_init_page(test_id, args, expected):
         (
             "root_page",
             wm.ROOT_PAGE,
-            wm.ROOT / "Home.md",
+            wm.WIKI_ROOT / "Home.md",
         ),
     ],
 )
