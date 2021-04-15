36f792422c2b9dd86ed25ae8b1a8d587ba8c8a0f
blakeNaccarato <20692450+blakeNaccarato@users.noreply.github.com>
2021-04-15 16:30:51 -0700

    Hardcode pages to break dependencies to module under test

--- tests/test_wikiman.py
+++ tests/test_wikiman.py
@@ -1,6 +1,7 @@
 # pylint: disable=missing-function-docstring, missing-module-docstring, redefined-outer-name, unused-argument, wrong-import-order
 
 from filecmp import dircmp
+from pathlib import Path
 
 import pytest
 import wikiman as wm
@@ -8,12 +9,39 @@ from pytest import mark as m
 
 from conftest import WIKI_ROOT
 
+
+# We hardcode our expected pages rather than pulling from the module under test. This
+# leads to some repetition, but avoids dependencies on module-level variables of the
+# module under test. We can map our "expected" results to this hardcoded set of pages
+# rather than burying a dependency to the module under test.
+PAGE_PATHS = {
+    # <name> : <path>
+    "Home": WIKI_ROOT,
+    # > Under "Home"
+    "Impeach-Vermilion-Vacuum": (IMPEACH := WIKI_ROOT / "00_Impeach-Vermilion-Vacuum"),
+    # >> Under "Impeach..."
+    "Measure-Transient-Respite": IMPEACH / "00_Measure-Transient-Respite",
+    "Official-Union-Advantage": (OFFICIAL := IMPEACH / "01_Official-Union-Advantage"),
+    # >>> Under "Official..."
+    "Close-Waste-Transform": OFFICIAL / "00_Close-Waste-Transform",
+    "Transit-Thrum-Middle": OFFICIAL / "01_Transit-Thrum-Middle",
+    "Serpentine-Harry-Butcher": OFFICIAL / "02_Serpentine-Harry-Butcher",
+    # >> Under "Impeach..."
+    "Middle-Pasture-Floating": IMPEACH / "02_Middle-Pasture-Floating",
+    # > Under "Home"
+    "Equity-Substitute-Huddle": (EQUITY := WIKI_ROOT / "01_Equity-Substitute-Huddle"),
+    # >> Under "Equity.."
+    "Automatic-Party-Merit": EQUITY / "00_Automatic-Party-Merit",
+    "Medium-Establish-Vital": EQUITY / "01_Medium-Establish-Vital",
+}
+
+PAGES = {str(key).lower(): value / (key + ".md") for key, value in PAGE_PATHS.items()}
+
 # ! -------------------------------------------------------------------------------- ! #
 # ! UTILITIES
 
 # * -------------------------------------------------------------------------------- * #
 # * STRINGS
-
 
 # * ---------------------------------------- * #
 # * get_dashed_name
@@ -78,17 +106,15 @@ def test_get_dir_name(test_id, args, expected):
 # * -------------------------------------------------------------------------------- * #
 # * MARKDOWN
 
-
 # * ---------------------------------------- * #
 # * get_page_url
-
 
 # We use `wm.GIT_REMOTE_URL` because we don't care about that part. That will resolve
 # the wiki repo properly when invoked by end-users. Sure, it doesn't give us a "proper"
 # wiki link in the test suite, since we're not invoking this from a "proper" wiki repo.
 # And we can't pytest.fixtures.monkeypatch a module-level variable, either.
 @m.parametrize(
-    "test_id, args, expected", [("base", (wm.ROOT_PAGE,), wm.GIT_REMOTE_URL + "Home")]
+    "test_id, args, expected", [("base", (PAGES["home"],), wm.GIT_REMOTE_URL + "Home")]
 )
 def test_get_page_url(test_id, args, expected):
     result = wm.get_page_url(*args)
@@ -111,7 +137,7 @@ def test_get_md_link(test_id, args, expected):
 
 @m.parametrize(
     "test_id, args, expected",
-    [("base", (wm.ROOT_PAGE,), f"[Home]({wm.GIT_REMOTE_URL}Home)")],
+    [("base", (PAGES["home"],), f"[Home]({wm.GIT_REMOTE_URL}Home)")],
 )
 def test_get_page_link(test_id, args, expected):
     result = wm.get_page_link(*args)
@@ -128,46 +154,25 @@ def test_bold_md(test_id, args, expected):
     assert result == expected
 
 
-# ! -------------------------------------------------------------------------------- ! #
-# ! API
-
 # * -------------------------------------------------------------------------------- * #
-# * RELATIVES
+# * PAGES
 
 # * ---------------------------------------- * #
-# * get_children
-
-
-@m.parametrize(
-    "test_id, args, expected",
-    [("root_page", (wm.ROOT_PAGE,), list(WIKI_ROOT.glob(f"*/{wm.PAGE_PATTERN}")))],
-)
-def test_get_children(test_id, args, expected):
-    result = wm.get_children(*args)
-    assert result == expected
-
-
-# * ---------------------------------------- * #
-# * get_parent
+# * find_page
 
 
 @m.parametrize(
     "test_id, args, expected",
     [
-        ("root_page", (wm.ROOT_PAGE,), WIKI_ROOT / "Home.md"),
-        ("subpage", (wm.PAGES[1],), WIKI_ROOT / "Home.md"),
+        ("root_page", ("Home",), PAGES["home"]),
+        ("lowercase", ("impeach-vermilion-vacuum",), PAGES["impeach-vermilion-vacuum"]),
+        ("uppercase", ("Impeach-Vermilion-Vacuum",), PAGES["impeach-vermilion-vacuum"]),
     ],
 )
-def test_get_parent(test_id, args, expected):
-    result = wm.get_parent(*args)
+def test_find_page(test_id, args, expected, RESTORE_WIKI_BEFORE_TEST):
+    result = wm.find_page(*args)
     assert result == expected
 
-
-# * -------------------------------------------------------------------------------- * #
-# * NAVIGATION
-
-# * -------------------------------------------------------------------------------- * #
-# * FILE OPERATIONS
 
 # * ---------------------------------------- * #
 # * init_page
@@ -176,22 +181,9 @@ def test_get_parent(test_id, args, expected):
 @m.parametrize(
     "test_id, args",
     [
-        # (
-        #     "backslash",
-        #     (
-        #         "Backs\lash",  # noqa:W605, pylint:disable=anomalous-backslash-in-string
-        #         wm.ROOT_PAGE,
-        #         0,
-        #     ),
-        # ),
-        (
-            "escape_sequences",
-            ("E\as\bc\fa\np\re\ts\v", wm.ROOT_PAGE, 0),
-        ),
-        (
-            "all_others",
-            ('/:*?"<>|', wm.ROOT_PAGE, 0),
-        ),
+        ("backslash", (r"Backs\lash", PAGES["home"], 0)),
+        ("escape_sequences", ("E\as\bc\fa\np\re\ts\v", PAGES["home"], 0)),
+        ("all_others", ('/:*?"<>|', PAGES["home"], 0)),
     ],
 )
 def test_init_page_raises(test_id, args):
@@ -207,8 +199,8 @@ INIT_PAGE_PARAMS = [
     # ),
     (
         "base",
-        ("test-page-name", wm.ROOT_PAGE, 0),
-        wm.WIKI_ROOT / "00_test-page-name/test-page-name.md",
+        ("test-page-name", PAGES["home"], 0),
+        WIKI_ROOT / "00_test-page-name/test-page-name.md",
     )
 ]
 
@@ -234,44 +226,75 @@ def test_init_page(test_id, args, expected):
 #     page = wm.init_page(*args)
 
 #     wm.create_page(page)
-#     result = dircmp(wm.WIKI_ROOT, EXPECTED_WIKI)
+#     result = dircmp(WIKI_ROOT, EXPECTED_WIKI)
 
 #     assert not result.diff_files
 
 
-# * ---------------------------------------- * #
-# * find_page
+# ! -------------------------------------------------------------------------------- ! #
+# ! API
 
-FIND_PAGE_PARAMS = [
-    # (
-    #     <test_id>
-    #     <args>
-    #     <expected>
-    # ),
+# * -------------------------------------------------------------------------------- * #
+# * RELATIVES
+
+# * ---------------------------------------- * #
+# * get_children
+
+PAGE_PATTERN = "[!_]*.md"
+
+
+@m.parametrize(
+    "test_id, args, expected",
+    [("root_page", (PAGES["home"],), list(WIKI_ROOT.glob(f"*/{PAGE_PATTERN}")))],
+)
+def test_get_children(test_id, args, expected):
+    result = wm.get_children(*args)
+    assert result == expected
+
+
+# * ---------------------------------------- * #
+# * get_parent
+
+
+@m.parametrize(
+    "test_id, args, expected",
+    [
+        ("root_page", (PAGES["home"],), PAGES["home"]),
+        ("subpage", (wm.find_page("impeach-vermilion-vacuum"),), PAGES["home"]),
+    ],
+)
+def test_get_parent(test_id, args, expected):
+    result = wm.get_parent(*args)
+    assert result == expected
+
+
+# * ---------------------------------------- * #
+# * get_siblings
+
+GET_SIBLINGS_PARAMS = [
     (
         "root_page",
-        (wm.ROOT_PAGE.stem,),
-        wm.ROOT_PAGE,
+        (PAGES["home"],),
+        EXPECTED_SIBLINGS := [
+            PAGES["impeach-vermilion-vacuum"],
+            PAGES["equity-substitute-huddle"],
+        ],
     ),
-    (
-        "lowercase",
-        ("impeach-vermilion-vacuum",),
-        FIND_PAGE_LOWERCASE_EXPECTED := (
-            WIKI_ROOT / r"00_Impeach-Vermilion-Vacuum\Impeach-Vermilion-Vacuum.md"
-        ),
-    ),
-    (
-        "uppercase",
-        ("Impeach-Vermilion-Vacuum",),
-        FIND_PAGE_LOWERCASE_EXPECTED,
-    ),
+    ("subpage", (PAGES["impeach-vermilion-vacuum"],), EXPECTED_SIBLINGS),
 ]
 
 
-# @m.parametrize("test_id, args, expected", FIND_PAGE_PARAMS)
-# def test_find_page(test_id, args, expected, RESTORE_WIKI_BEFORE_TEST):
-#     result = wm.find_page(*args)
-#     assert result == expected
+@m.parametrize("test_id, args, expected", GET_SIBLINGS_PARAMS)
+def test_get_siblings(test_id, args, expected):
+    result = wm.get_siblings(*args)
+    assert result == expected
+
+
+# * -------------------------------------------------------------------------------- * #
+# * NAVIGATION
+
+# * -------------------------------------------------------------------------------- * #
+# * FILE OPERATIONS
 
 
 # ! -------------------------------------------------------------------------------- ! #
