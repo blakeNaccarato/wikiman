74a3d250716915e59040fc166d0879c50d7baf78
Blake Naccarato <20692450+blakeNaccarato@users.noreply.github.com>
2021-04-17 18:31:19 -0700

    Update setup.py

--- setup.py
+++ setup.py
@@ -18,7 +18,7 @@ setup(
     package_dir={"": "src"},
     packages=find_packages(where="src"),
     python_requires=">=3.8",
-    install_requires=["fire", "markdown", "GitPython"],
+    install_requires=["fire==0.4.0", "markdown==3.3.4", "GitPython==3.1.14"],
     entry_points={
         "console_scripts": ["wikiman=wikiman.wikiman:main"],
     },
