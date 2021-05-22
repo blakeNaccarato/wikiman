2b667b2e66d15a01d5aa7cecb9014c266594db19
blakeNaccarato <20692450+blakeNaccarato@users.noreply.github.com>
2021-05-21 20:14:10 -0700

    Implement basic structure of `Page` class

--- src/wikiman/family.py
+++ src/wikiman/family.py
@@ -1,8 +1,45 @@
 """Functions that find the family of a page."""
 
+from __future__ import annotations  # postponed evaluation of annotations
+
 from pathlib import Path
 
 from wikiman import common
+
+PAGE_PATTERN = "[!_]*.md"
+
+# The origin repo should be a GitHub wiki, and pages should be in the "wiki" subfolder
+ROOT_NAME = "wiki"
+WIKI_ROOT = Path(ROOT_NAME)
+
+
+def init_wiki(wiki_root: Path = WIKI_ROOT):
+    if not WIKI_ROOT.exists():
+        WIKI_ROOT.mkdir()
+        (WIKI_ROOT / "Home.md").touch()
+
+
+def get_pages(wiki_root: Path = WIKI_ROOT) -> list[Page]:
+    paths = sorted(wiki_root.glob(f"**/{PAGE_PATTERN}"))
+    return [Page(path) for path in paths]
+
+
+class Page:
+    def __init__(self, path: Path):
+        self.path = path
+        self.name = path.stem
+
+    def make(self):
+        self.mkdir()
+        self.path.touch()
+
+    def mkdir(self):
+        if not self.path.exists():
+            self.path.mkdir()
+
+
+# * -------------------------------------------------------------------------------- * #
+# * FAMILY
 
 
 def get_siblings(page: Path) -> list[Path]:
