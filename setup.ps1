d2b71fe5c814a8c55a22631ede5a30487d826485
blakeNaccarato <20692450+blakeNaccarato@users.noreply.github.com>
2021-09-18 16:13:55 -0700

    Perform initial copy operation.
    We still have to makesure gitignored files get ignored.

---
+++ setup.ps1
@@ -0,0 +1,6 @@
+copier gh:blakeNaccarato/copier-python . -r 7a6c90d
+py -3.9 -m venv .venv
+.venv/Scripts/activate
+pip install -U pip  # throws [WinError 5], but still works on its own
+pip install -r requirements_dev.txt  # packages for development
+flit install -s
