bdf57d9ffe473990131e3398838bf88b95a69743
blakeNaccarato <20692450+blakeNaccarato@users.noreply.github.com>
2021-09-19 14:14:12 -0700

    Bump tooling

--- setup.ps1
+++
@@ -1,7 +0,0 @@
-copier -r 35e97fb
-py -3.9 -m venv .venv
-.venv/Scripts/activate
-pip install -U pip  # throws [WinError 5], but still works on its own
-pip install wheel
-pip install -r requirements_dev.txt
-flit install -s
--- setup_cicd.sh
+++
@@ -1,7 +0,0 @@
-git reset --hard
-pip install -U pip wheel
-pip install pipx
-pipx install copier==6.0.0a7
-copier -f -r 35e97fb
-pip install -r requirements_cicd.txt
-flit install
